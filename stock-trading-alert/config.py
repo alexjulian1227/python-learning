16b75c51211bf3891fa56454798b03d4c1b576fb
Alex Julian <95150394+alexjulian1227@users.noreply.github.com>
2022-01-03 05:48:51 +0800

    Create config.py

---
+++ stock-trading-alert/config.py
@@ -0,0 +1,5 @@
+
+NEWS_API_KEY = "please use your api key:link in main.py"
+ALPHA_API_KEY = "please use your api key:link in main.py"
+
+# i was working on with config lib and started it out. will try to read the documentation
